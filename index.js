57905f12ee2f1db63c2f5abb3b978f316057b64a
golam-kibriya123 <golamkibriya622@gmail.com>
2023-06-15 22:34:26 -0500

    added jwt

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -12,16 +14,25 @@ app.use(express.json());
 // database
 const pass = process.env.DB_PASS;
 const user = process.env.DB_USER;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${user}:${pass}@cluster0.wrk7b1c.mongodb.net/?retryWrites=true&w=majority`;
 
+
+app.post('/jwt', (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+    res.send({ token })
+})
+app.get('/jwt', (req, res) => {
+    res.send('ok')
+})
 app.get('/', (req, res) => {
     res.send('drawing club is running ');
     console.log(pass)
     console.log(user)
 });
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -32,10 +43,11 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect();
         const classesCollection = client.db('drawingDB').collection('classes');
         const instructorsCollection = client.db('drawingDB').collection('instructors');
         const studentsCollection = client.db('drawingDB').collection('students');
+        const usersCollection = client.db('drawingDB').collection('users');
 
         app.get('/classes', async (req, res) => {
             const query = {};
@@ -43,6 +55,91 @@ async function run() {
             const result = await classesCollection.find({}).toArray();
             res.send(result)
         });
+        app.get('/classes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await classesCollection.find(query).toArray();
+            res.send(result)
+
+        });
+        app.patch('/classes/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updateClass = req.body;
+            const update = {
+                $set: {
+                    price: updateClass.price,
+                    seat: updateClass.price,
+                    class_photo: updateClass.class_photo,
+                    class_name: updateClass.class_name,
+
+                }
+            };
+
+
+
+            const result = await classesCollection.updateOne(filter, update, options);
+            res.send(result)
+        })
+
+        app.post('/classes', async (req, res) => {
+            const added_class = req.body;
+            const result = await classesCollection.insertOne(added_class);
+            res.send(result)
+
+        });
+
+
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result)
+        });
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const filter = { _id: new ObjectId(id) };
+            const updateRole = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateRole);
+            res.send(result)
+        });
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const filter = { _id: new ObjectId(id) };
+            const updateRole = {
+                $set: {
+                    role: 'instructor'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateRole);
+            res.send(result)
+        });
+        app.get('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const result = await usersCollection.find(filter).toArray();
+            res.send(result)
+        })
+        app.post('/users', async (req, res) => {
+            const add_user = req.body;
+            const query = { email: add_user.email };
+            const checkUser = await usersCollection.findOne(query);
+            if (checkUser) {
+                return res.send({ message: 'the user already added' });
+            }
+            const result = await usersCollection.insertOne(add_user);
+            res.send(result)
+
+        })
+
+
+
+
         app.get('/popularclass', async (req, res) => {
             const query = {}
             const option = { sort: { "enrolled": -1 }, };
@@ -56,7 +153,7 @@ async function run() {
             res.send(result)
         });
         app.get('/popularinstructors', async (req, res) => {
-            
+
             const option = {
                 sort: { "enrolled_student": 1 }
             }
@@ -75,13 +172,6 @@ async function run() {
 
 
 
-
-
-
-
-
-
-
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -91,8 +181,7 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
-
 app.listen(port, () => {
     console.log('the port is running on port', port)
 })
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
